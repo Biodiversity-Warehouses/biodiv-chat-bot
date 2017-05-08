7f881031ad84ba7388c80f891233eed005ecafe1
Stefan B <Stefan@dhcp-vl203-10-7.hs-bremen.de>
2017-05-08 14:20:52 +0200

    - add facebook bot

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Bot = require('messenger-bot');
+
+let bot = new Bot({
+  token: process.env.TOKEN,
+  verify: process.env.VERIFY,
+  app_secret: process.env.APP_SECERT
+});
+
+bot.on('error', (err) => {
+  console.log(err.message)
+});
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text;
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
+
+    reply({ text }, (err) => {
+      if (err) throw err;
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+});
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  return bot._verify(req, res)
+});
+
+app.post('/', (req, res) => {
+  bot._handleMessage(req.body);
+  res.end(JSON.stringify({status: 'ok'}))
+});
+
+http.createServer(app).listen(3000);
