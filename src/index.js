cf92dc9838d1d28b2ef668431bc1eb71d4dc09fa
Stefan B <Stefan@MacBook-Pro-5.fritz.box>
2017-05-08 15:17:27 +0200

    - changed back

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-'use strict';
-const BootBot = require('bootbot');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Bot = require('messenger-bot');
 
-const port = process.env.PORT | 3000;
+let port = process.env.PORT || 8080;
 
-const bot = new BootBot({
-  accessToken: process.env.TOKEN,
-  verifyToken: process.env.VERIFY,
-  appSecret: process.env.APP_SECRET
+let bot = new Bot({
+  token: process.env.TOKEN,
+  verify: process.env.VERIFY,
+  app_secret: process.env.APP_SECERT
 });
 
-bot.on('message', (payload, chat) => {
-  const text = payload.message.text;
-  chat.say(`Echo: ${text}`);
+bot.on('error', (err) => {
+  console.log(err.message)
 });
 
-bot.start(port);
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text;
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
+
+    reply({ text }, (err) => {
+      if (err) throw err;
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+});
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  return bot._verify(req, res)
+});
+
+
+app.get('/hello', (req, res) => {
+  return res.send(JSON.stringify({status: 'hello'}))
+});
+
+
+
+app.post('/', (req, res) => {
+  bot._handleMessage(req.body);
+  res.end(JSON.stringify({status: 'ok'}))
+});
+
+http.createServer(app).listen(port, ()=>{
+  console.log("Server is up and running on port: " + port)
+});
